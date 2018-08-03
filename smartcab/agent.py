69adb1d4f9b68c4272ef546f0f7043fc86a2b710
iyunbo <yunbo.wang@solven.eu>
2018-08-03 22:08:34 +0200

    question 5 update: 更新驾驶代理程序的状态

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -56,7 +56,7 @@ class LearningAgent(Agent):
         ## TO DO ##
         ###########
         # Set 'state' as a tuple of relevant data for the agent        
-        state = None
+        state = (waypoint, inputs['light'], inputs['oncoming'], inputs['right'], inputs['left'], deadline)
 
         return state
 

4b24eaa6b9422a1924769a071b7a29f44913d013
iyunbo <yunbo.wang@solven.eu>
2018-08-04 21:20:41 +0200

    Q-Learning模拟结果: optimized

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -190,7 +190,7 @@ def run():
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05 
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run(n_test=100, tolerance=0.005)
+    sim.run(n_test=100)
 
 
 if __name__ == '__main__':

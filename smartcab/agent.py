9bc7cc57db7886da9d8a352e367ce936b1443d4e
iyunbo <yunbo.wang@solven.eu>
2018-08-04 18:39:40 +0200

    Q-Learning模拟结果: optimized

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -170,7 +170,7 @@ def run():
     #   learning   - set to True to force the driving agent to use Q-learning
     #    * epsilon - continuous value for the exploration factor, default is 1
     #    * alpha   - continuous value for the learning rate, default is 0.5
-    agent = env.create_agent(LearningAgent, learning=True, epsilon=0.99, alpha=0.6)
+    agent = env.create_agent(LearningAgent, learning=True, alpha=0.8)
 
     ##############
     # Follow the driving agent

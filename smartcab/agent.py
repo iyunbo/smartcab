44bdc02f74580efc04cee0172cabc7c8a4dd4d05
iyunbo <yunbo.wang@solven.eu>
2018-08-04 18:48:12 +0200

    Q-Learning模拟结果: optimized

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -45,7 +45,7 @@ class LearningAgent(Agent):
             self.epsilon = 0
             self.alpha = 0
         else:
-            self.epsilon = self.epsilon - 0.05
+            self.epsilon = math.exp(-0.02 * self.t)
             self.t += 1
 
         return None
@@ -64,7 +64,7 @@ class LearningAgent(Agent):
         ## TO DO ##
         ###########
         # Set 'state' as a tuple of relevant data for the agent        
-        state = (waypoint, inputs['light'], inputs['right'], inputs['oncoming'], deadline)
+        state = (waypoint, inputs['light'], inputs['right'], inputs['oncoming'])
 
         return state
 

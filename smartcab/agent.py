e6f5af33e2cdb31ce3e6a23c95b619ae54635e1c
iyunbo <yunbo.wang@solven.eu>
2018-08-05 02:01:02 +0200

    Q-Learning模拟结果: optimized

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -49,7 +49,7 @@ class LearningAgent(Agent):
             self.epsilon = math.cos(0.005 * self.t)
             self.t += 1
             if self.alpha > 0.001:
-                self.alpha *= 0.99
+                self.alpha *= 0.999
 
         return None
 

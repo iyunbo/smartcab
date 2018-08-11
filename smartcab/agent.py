b522e0ab74d840202507359c7da1fc7740db78eb
iyunbo <yunbo.wang@solven.eu>
2018-08-11 20:59:40 +0200

    visually testing Q-Learning's optimal strategy

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -46,7 +46,7 @@ class LearningAgent(Agent):
             self.epsilon = 0
             self.alpha = 0
         else:
-            self.epsilon = math.cos(0.002 * self.t)
+            self.epsilon = math.cos(0.0015 * self.t)
             self.t += 1
 
         return None

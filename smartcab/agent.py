acdafb5e86ea841b74669f7fb04fd220262ea6cb
iyunbo <yunbo.wang@solven.eu>
2018-08-11 23:09:28 +0200

    visually testing Q-Learning's optimal strategy

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -46,8 +46,8 @@ class LearningAgent(Agent):
             self.epsilon = 0
             self.alpha = 0
         else:
-            self.epsilon = self.epsilon - 0.05
-            # self.t += 1
+            self.epsilon = math.cos(0.0015 * self.t)
+            self.t += 1
 
         return None
 
@@ -202,7 +202,7 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env, update_delay=0.001, display=False, log_metrics=True, optimized=False)
+    sim = Simulator(env, update_delay=0.001, display=False, log_metrics=True, optimized=True)
 
     ##############
     # Run the simulator
